49a7d7e588e896d263a312cc2b35676483cda896
Tomasz Łaski <tomasz.laski0001@gmail.com>
2019-12-04 20:27:00 +0100

    task 17.5 -routing and redux implementation

--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -11,11 +11,15 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
-
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
-
+export const addTag = payload => ({ payload, type: ADD_TAG });
+export const removeTag = payload => ({ payload, type: REMOVE_TAG });
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
 // reducer
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
@@ -25,7 +29,34 @@ export default function reducer(statePart = [], action = {}) {
         searchPhrase: action.payload,
       };
     // TODO - handle other action types
+    case ADD_TAG:
+      return {
+        ...statePart,
+        tags: [...statePart.tags, action.payload],
+      };
+    case REMOVE_TAG:
+
+      // eslint-disable-next-line no-case-declarations
+      const index = statePart.tags.indexOf(action.payload);
+      return {
+        ...statePart,
+        tags: [
+          ...statePart.tags.slice(0, index),
+          ...statePart.tags.slice(index + 1),
+        ],
+      };
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          ...action.payload,
+        },
+      };
     default:
       return statePart;
   }
 }
+
+
+
