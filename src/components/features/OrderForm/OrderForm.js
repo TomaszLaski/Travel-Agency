cbed0dfd2865b8f741c10906d40c091c8db991b9
Tomasz Łaski <tomasz.laski0001@gmail.com>
2020-03-10 15:48:18 +0100

    task 19.4

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,6 +4,40 @@ import OrderSummary from '../OrderSummary/OrderSummary.js';
 import PropTypes from 'prop-types';
 import OrderOption from '../OrderOption/OrderOption.js';
 import Pricing from '../../../data/pricing.json';
+import Button from '../../common/Button/Button';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost, tripName, tripId, CountryCode) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripName, 
+    tripId, 
+    CountryCode,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm =  (props) => (
   // 18.3 wyświetlanie pojedynczej opcji:
@@ -18,7 +52,8 @@ const OrderForm =  (props) => (
     ))}
     <Col xs={12}>
       <OrderSummary tripCost={props.tripCost} options={props.options} />
-    </Col>
+    </Col>    
+    <Button onClick={() => sendOrder(props.options, props.tripCost, props.tripName, props.tripId, props.CountryCode)}>Order now!</Button>
   </Row>
 );
 
@@ -26,6 +61,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripName: PropTypes.string.isRequired, 
+  tripId: PropTypes.string.isRequired, 
+  CountryCode: PropTypes.string.isRequired,
 };
 
 export default OrderForm;
